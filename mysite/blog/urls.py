cba15319d553a77ce2c596aa9a929e21930b994f
Falzhan <zhandermontefalcon@gmail.com>
2025-12-27 14:38:02 +0800

    template fixes

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import blog_list, blog_detail, blog_delete, blog_create, blog_update
 
 urlpatterns = [
-   path('',blog_list),
-   path('create/',blog_create),
-   path('<int:id>/update/',blog_update),
-   path('<int:id>/',blog_detail),
-   path('<int:id>/delete/',blog_delete)
-
+    path('create/', blog_create, name='blog_create'),
+    path('<int:id>/update/', blog_update, name='blog_update'),
+    path('<int:id>/', blog_detail, name='blog_detail'),
+    path('<int:id>/delete/', blog_delete, name='blog_delete'),
+    path('posts/', blog_list, name='blog_list'),
+    path('', blog_list),
 ]
